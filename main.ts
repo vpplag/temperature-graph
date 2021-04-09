e620f9b548a65175309d953f9339f8f7352827b1
vpplag <74141971+vpplag@users.noreply.github.com>
2021-04-09 17:14:55 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    button_pressed = 1
+})
+input.onButtonPressed(Button.AB, function () {
+    button_pressed = 0
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    button_pressed = 2
+})
+let button_pressed = 0
+button_pressed = 0
 basic.forever(function () {
-    led.plotBarGraph(
-    input.temperature(),
-    40
-    )
+    if (button_pressed == 1) {
+        led.plotBarGraph(
+        input.temperature(),
+        40
+        )
+    } else if (button_pressed == 2) {
+        led.plotBarGraph(
+        input.lightLevel(),
+        255
+        )
+    }
 })
